0de53cb08a647d8d39526efab3e086338cc941b3
Lynn McQueen <lynn.edward.mcqueen@gmail.com>
2024-01-31 15:58:07 -0700

    Added the meta data for seo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import Header from "./component/Header";
+import Main from "./component/Main";
+import Nav from "./component/Nav";
+import Footer from "./component/Footer"
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      HomePage
-    </div>
+    <>
+      <Nav />
+      <Header />
+      <Main />
+      <Footer />      
+    </>
   );
 }
 
---
+++ src/component/Footer.js
@@ -0,0 +1,38 @@
+import "./Footer.css"
+import logo from "./../assets/images/Logo.svg"
+
+function Footer() {
+    return (
+        <footer>
+            <ul>
+                <li>
+                    <img id = "rotate" src = {logo} width = "221" height = "290" alt = "Big Logo" />
+                </li>
+                <li>
+                    <a href = "#">Home</a>
+                </li>
+                <li>
+                    <a href = "#">About</a>
+                </li>
+                <li>
+                    <a href = "#">Menu</a>
+                </li>
+                <li>
+                    <a href = "#">Reservations</a>
+                </li>
+                <li>
+                    <a href = "#">Order Online</a>
+                </li>
+                <li>
+                    <a href = "#">Login</a>
+                </li>
+  
+
+                
+            </ul>
+            Footer
+        </footer>
+    )
+}
+
+export default Footer;
